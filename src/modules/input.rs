e06c27997dffd764b84eda1e06532ddff6e93113
NomisIV <simon@nomisiv.com>
2021-06-17 19:54:22 +0200

    Change input module to use enum for input sources

--- src/modules/input.rs
+++ src/modules/input.rs
@@ -3,81 +3,20 @@ use structopt::StructOpt;
 use super::Module;
 use super::{Zone, Zoned};
 
+mod sources;
+use sources::{Sources, ToCode};
+
 #[derive(Debug, StructOpt)]
 pub enum InputOpt {
     Next,
     Prev,
-    Set { source: String },
+    Set { source: Sources }, // This only supports non-zoned sources for now
     Query,
 }
 
 pub struct InputModule;
 
 impl InputModule {
-    // TODO: Convert these to FromStr to a Source enum?
-    pub fn match_source(source: &str) -> String {
-        format!(
-            "{:0>2}",
-            match source {
-                "bd" => 25,
-                "dvd" => 4,
-                "sat/cbl" => 6,
-                "dvr/bdr" => 15,
-                "hdmi1" => 19,
-                "hdmi2" => 20,
-                "hdmi3" => 21,
-                "hdmi4" => 22,
-                "hdmi5" => 23,
-                "hdmi6" => 24,
-                "hdmi7" => 34,
-                "network" => 26,
-                "internet-radio" => 38,
-                "spotify" => 53, // Is this even supported anymore?
-                "pandora" => 41,
-                "media-server" => 44,
-                "favorites" => 45,
-                "ipod-usb" => 17,
-                "tv" => 5,
-                "cd" => 1,
-                "usb-dac" => 13,
-                "tuner" => 2,
-                "phono" => 0,
-                "multi-channel" => 12,
-                "bluetooth" => 33, // BT AUDIO
-                "hdmi" => 31,      // HDMI (cyclic)
-                "airplay" => 46,   // Information only
-                "dmr" => 47,       // Information only
-                _ => 99, // ERROR
-            }
-        )
-    }
-
-    pub fn match_source_zoned(source: &str) -> String {
-        format!(
-            "{:0>2}",
-            match source {
-                "dvd" => 4,
-                "sat/cbl" => 6,
-                "dvr/bdr" => 15,
-                "network" => 26,
-                "internet-radio" => 38,
-                "spotify" => 53, // Is this even supported anymore?
-                "pandora" => 41,
-                "media-server" => 44,
-                "favorites" => 45,
-                "ipod-usb" => 17,
-                "usb-dac" => 13,
-                "tv" => 5,
-                "cd" => 1,
-                "tuner" => 2,
-                "bluetooth" => 33, // BT AUDIO
-                "airplay" => 46,   // Information only
-                "dmr" => 47,       // Information only
-                _ => 99, // ERROR
-            }
-        )
-    }
-
     pub fn parse_command(cmd: &InputOpt) -> String {
         let code = InputModule::get_code(&Zone::Main);
 
@@ -85,18 +24,9 @@ impl InputModule {
             InputOpt::Next => format!("{}U", code),
             InputOpt::Prev => format!("{}D", code),
             InputOpt::Set { source } => match Zone::Main {
-                Zone::Main => {
-                    let source = InputModule::match_source(source);
-                    format!("{}{}N", source, code)
-                }
-                Zone::HDZone => {
-                    let source = InputModule::match_source_zoned(source);
-                    format!("{}{}A", source, code)
-                }
-                _ => {
-                    let source = InputModule::match_source_zoned(source);
-                    format!("{}{}", source, code)
-                }
+                Zone::Main => format!("{}{}N", source.to_code(), code),
+                Zone::HDZone => format!("{}{}A", source.to_code(), code),
+                _ => format!("{}{}", source.to_code(), code),
             },
             InputOpt::Query => format!("?{}", &code),
         }
---
+++ src/modules/input/sources.rs
@@ -0,0 +1,192 @@
+use std::str::FromStr;
+use structopt::clap::Error;
+
+pub trait ToCode {
+    fn to_code(&self) -> u8;
+}
+
+#[derive(Debug)]
+pub enum Sources {
+    BD,
+    DVD,
+    SatCbl,
+    DvrBdr,
+    HDMI1,
+    HDMI2,
+    HDMI3,
+    HDMI4,
+    HDMI5,
+    HDMI6,
+    HDMI7,
+    Network,
+    InternetRadio,
+    Spotify,
+    Pandora,
+    MediaServer,
+    Favorites,
+    IpodUsb,
+    TV,
+    CD,
+    UsbDac,
+    Tuner,
+    Phono,
+    MultiChannel,
+    Bluetooth,
+    HDMI,
+    Airplay,
+    DMR,
+}
+
+impl FromStr for Sources {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "bd" => Ok(Self::BD),
+            "dvd" => Ok(Self::DVD),
+            "sat-cbl" => Ok(Self::SatCbl),
+            "dvr-bdr" => Ok(Self::DvrBdr),
+            "hdmi-1" => Ok(Self::HDMI1),
+            "hdmi-2" => Ok(Self::HDMI2),
+            "hdmi-3" => Ok(Self::HDMI3),
+            "hdmi-4" => Ok(Self::HDMI4),
+            "hdmi-5" => Ok(Self::HDMI5),
+            "hdmi-6" => Ok(Self::HDMI6),
+            "hdmi-7" => Ok(Self::HDMI7),
+            "network" => Ok(Self::Network),
+            "internet-radio" => Ok(Self::InternetRadio),
+            "spotify" => Ok(Self::Spotify),
+            "pandora" => Ok(Self::Pandora),
+            "media-server" => Ok(Self::MediaServer),
+            "favorites" => Ok(Self::Favorites),
+            "ipod-usb" => Ok(Self::IpodUsb),
+            "tv" => Ok(Self::TV),
+            "cd" => Ok(Self::CD),
+            "usb-dac" => Ok(Self::UsbDac),
+            "tuner" => Ok(Self::Tuner),
+            "phono" => Ok(Self::Phono),
+            "multi-channel" => Ok(Self::MultiChannel),
+            "bluetooth" => Ok(Self::Bluetooth),
+            "hdmi" => Ok(Self::HDMI),
+            "airplay" => Ok(Self::Airplay),
+            "dmr" => Ok(Self::DMR),
+            _ => Err(Error {
+                message: format!("{} is not an input source", s),
+                kind: structopt::clap::ErrorKind::InvalidValue,
+                info: None,
+            }),
+        }
+    }
+}
+
+impl ToCode for Sources {
+    fn to_code(&self) -> u8 {
+        match self {
+            Self::BD => 25,
+            Self::DVD => 4,
+            Self::SatCbl => 6,
+            Self::DvrBdr => 15,
+            Self::HDMI1 => 19,
+            Self::HDMI2 => 20,
+            Self::HDMI3 => 21,
+            Self::HDMI4 => 22,
+            Self::HDMI5 => 23,
+            Self::HDMI6 => 24,
+            Self::HDMI7 => 25,
+            Self::Network => 26,
+            Self::InternetRadio => 38,
+            Self::Spotify => 53,
+            Self::Pandora => 41,
+            Self::MediaServer => 44,
+            Self::Favorites => 45,
+            Self::IpodUsb => 17,
+            Self::TV => 5,
+            Self::CD => 1,
+            Self::UsbDac => 13,
+            Self::Tuner => 2,
+            Self::Phono => 0,
+            Self::MultiChannel => 12,
+            Self::Bluetooth => 33,
+            Self::HDMI => 31,
+            Self::Airplay => 46,
+            Self::DMR => 47,
+        }
+    }
+}
+
+// SourcesZoned enum is a subset of Sources enum
+#[derive(Debug)]
+pub enum SourcesZoned {
+    DVD,
+    SatCbl,
+    DvrBdr,
+    Network,
+    InternetRadio,
+    Spotify,
+    Pandora,
+    MediaServer,
+    Favorites,
+    IpodUsb,
+    UsbDac,
+    TV,
+    CD,
+    Tuner,
+    Bluetooth,
+    Airplay,
+    DMR,
+}
+
+impl FromStr for SourcesZoned {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "dvd" => Ok(Self::DVD),
+            "sat-cbl" => Ok(Self::SatCbl),
+            "dvr-bdr" => Ok(Self::DvrBdr),
+            "network" => Ok(Self::Network),
+            "internet-radio" => Ok(Self::InternetRadio),
+            "spotify" => Ok(Self::Spotify),
+            "pandora" => Ok(Self::Pandora),
+            "media-server" => Ok(Self::MediaServer),
+            "favorites" => Ok(Self::Favorites),
+            "ipod-usb" => Ok(Self::IpodUsb),
+            "tv" => Ok(Self::TV),
+            "cd" => Ok(Self::CD),
+            "usb-dac" => Ok(Self::UsbDac),
+            "tuner" => Ok(Self::Tuner),
+            "bluetooth" => Ok(Self::Bluetooth),
+            "airplay" => Ok(Self::Airplay),
+            "dmr" => Ok(Self::DMR),
+            _ => Err(Error {
+                message: format!("{} is not a zoned input source", s),
+                kind: structopt::clap::ErrorKind::InvalidValue,
+                info: None,
+            }),
+        }
+    }
+}
+
+impl ToCode for SourcesZoned {
+    fn to_code(&self) -> u8 {
+        match self {
+            Self::DVD => 4,
+            Self::SatCbl => 6,
+            Self::DvrBdr => 15,
+            Self::Network => 26,
+            Self::InternetRadio => 38,
+            Self::Spotify => 53,
+            Self::Pandora => 41,
+            Self::MediaServer => 44,
+            Self::Favorites => 45,
+            Self::IpodUsb => 17,
+            Self::TV => 5,
+            Self::CD => 1,
+            Self::UsbDac => 13,
+            Self::Tuner => 2,
+            Self::Bluetooth => 33,
+            Self::Airplay => 46,
+            Self::DMR => 47,
+        }
+    }
+}
